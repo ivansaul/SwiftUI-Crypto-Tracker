bc51151a71be4c63cc7ae792a5ebfa86d479c3df
ivansaul <saul_2503@hotmail.com>
2024-06-24 02:16:10 -0500

    feat: Display downloaded detail coin data in grid format

--- SwiftUICrypto/Core/Detail/ViewModels/CoinDetailViewModel.swift
+++ SwiftUICrypto/Core/Detail/ViewModels/CoinDetailViewModel.swift
@@ -9,7 +9,8 @@ import Combine
 import Foundation
 
 class CoinDetailViewModel: ObservableObject {
-    @Published var coinDetail: CoinDetailModel? = nil
+    @Published var overviewStatistics: [StatisticModel] = []
+    @Published var aditionalStatistics: [StatisticModel] = []
 
     private let coinDetailDataService: CoinDetailDataService
 
@@ -25,9 +26,58 @@ class CoinDetailViewModel: ObservableObject {
 
     private func addSubscribers() {
         self.coinDetailDataService.$coinDetail
-            .sink { [weak self] returnedCoinDetail in
-                self?.coinDetail = returnedCoinDetail
-            }
+            .map(self.mapDataToStatistics)
+            .sink(receiveValue: { [weak self] returnedOverviewStats, returnedAditionalStats in
+                self?.overviewStatistics = returnedOverviewStats
+                self?.aditionalStatistics = returnedAditionalStats
+            })
             .store(in: &self.cancellables)
+    }
+
+    private func mapDataToStatistics(coinDetailModel: CoinDetailModel?) -> (overview: [StatisticModel], aditional: [StatisticModel]) {
+        // OVERVIEW
+
+        let price = self.coin.currentPrice.asCurrencyWithDecimals()
+        let pricePercent = self.coin.priceChangePercentage24h
+        let priceStat = StatisticModel(title: "Current Price", value: price, precentageChange: pricePercent)
+
+        let marketCap = "$" + ((self.coin.marketCap?.formattedWithAbbreviations()) ?? "")
+        let marketCapPercent = self.coin.marketCapChangePercentage24h
+        let marketCapStat = StatisticModel(title: "Market Cap.", value: marketCap, precentageChange: marketCapPercent)
+
+        let rank = String(self.coin.rank)
+        let rankStat = StatisticModel(title: "Rank", value: rank)
+
+        let volume = "$" + (self.coin.totalVolume?.formattedWithAbbreviations() ?? "")
+        let volumeStat = StatisticModel(title: "Volume", value: volume)
+
+        let overviewStats = [priceStat, marketCapStat, rankStat, volumeStat]
+
+        // ADITIONAL
+
+        let height = self.coin.high24h?.asCurrencyWithDecimals() ?? ""
+        let heightStat = StatisticModel(title: "24h Height", value: height)
+
+        let low = self.coin.low24h?.asCurrencyWithDecimals() ?? ""
+        let lowStat = StatisticModel(title: "24h Low", value: low)
+
+        let priceChange = self.coin.priceChange24h?.asCurrencyWithDecimals() ?? ""
+        let priceChangePercent = self.coin.priceChangePercentage24h
+        let priceChangeStat = StatisticModel(title: "24h Price Change", value: priceChange, precentageChange: priceChangePercent)
+
+        let marketCapChange = "$" + ((self.coin.marketCapChange24h?.formattedWithAbbreviations()) ?? "")
+        let marketCapChangePercent = self.coin.marketCapChangePercentage24h
+        let marketCapChangeStat = StatisticModel(title: "24h Market Cap. Change", value: marketCapChange, precentageChange: marketCapChangePercent)
+
+        let blockTime = coinDetailModel?.blockTimeInMinutes ?? 0
+        let blockTimeString = blockTime == 0 ? "n/a" : String(blockTime)
+        let blocTimeStat = StatisticModel(title: "Block Time", value: blockTimeString)
+
+        let hashing = coinDetailModel?.hashingAlgorithm ?? "n/a"
+        let hashingStat = StatisticModel(title: "Hashing Algorithm", value: hashing)
+
+        let aditionalStats = [heightStat, lowStat, priceChangeStat, marketCapChangeStat, blocTimeStat, hashingStat]
+
+        return (overviewStats, aditionalStats)
     }
 }
--- SwiftUICrypto/Core/Detail/Views/DetailsView.swift
+++ SwiftUICrypto/Core/Detail/Views/DetailsView.swift
@@ -10,18 +10,69 @@ import SwiftUI
 struct DetailsView: View {
     @ObservedObject private var detailVM: CoinDetailViewModel
 
+    private let gridColumns = [GridItem(.flexible()), GridItem(.flexible())]
+    private let gridSpacing: CGFloat = 30
+
     init(coin: CoinModel) {
         _detailVM = ObservedObject(wrappedValue: CoinDetailViewModel(coin: coin))
     }
 
     var body: some View {
-        Text(detailVM.coin.id)
-        if let coinId = detailVM.coinDetail?.description?.en {
-            Text(coinId)
+        ScrollView {
+            VStack {
+                // TODO: Implement ChartView
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(.gray.opacity(0.3))
+                    .frame(height: 250)
+                overViewSection
+                aditionalDetailsSection
+            }
+            .padding(.horizontal)
         }
+        .navigationTitle(detailVM.coin.name)
     }
 }
 
 #Preview {
-    DetailsView(coin: DeveleperPreview.instance.coin)
+    NavigationView {
+        DetailsView(coin: DeveleperPreview.instance.coin)
+    }
+}
+
+extension DetailsView {
+    private var overViewSection: some View {
+        VStack {
+            Text("Overview".uppercased())
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundStyle(Color.theme.accent)
+                .frame(maxWidth: .infinity, alignment: .leading)
+
+            Divider()
+
+            LazyVGrid(columns: gridColumns, alignment: .leading, spacing: gridSpacing, content: {
+                ForEach(detailVM.overviewStatistics) { stat in
+                    StatisticView(stat: stat)
+                }
+            })
+        }
+    }
+
+    private var aditionalDetailsSection: some View {
+        VStack {
+            Text("Aditional Details".uppercased())
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundStyle(Color.theme.accent)
+                .frame(maxWidth: .infinity, alignment: .leading)
+
+            Divider()
+
+            LazyVGrid(columns: gridColumns, alignment: .leading, spacing: gridSpacing, content: {
+                ForEach(detailVM.aditionalStatistics) { stat in
+                    StatisticView(stat: stat)
+                }
+            })
+        }
+    }
 }
